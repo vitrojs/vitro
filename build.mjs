e45b9cd7f85d180469537c8c5556745b12a5ffe1
dong.huo <huodon@gmail.com>
2024-01-07 23:19:08 +0800

    fix build script

--- build.mjs
+++ build.mjs
@@ -1,7 +1,7 @@
 import { buildSync } from 'esbuild'
 import { printDone, rmDist } from '../../scripts/build-utils.mjs'
 
-rmDist()
+rmDist(import.meta.url)
 
 buildSync({
   entryPoints: ['src/index.ts', 'src/jsx/runtime.ts'],
@@ -14,3 +14,22 @@ buildSync({
 })
 
 printDone()
+
+
+
+import * as path from 'node:path'
+import * as fs from 'node:fs'
+import * as url from 'node:url'
+
+export function rmDist(importUrl) {
+  const dirname = path.dirname(url.fileURLToPath(importUrl))
+  const dist = path.join(dirname, 'dist')
+  if (fs.existsSync(dist)) {
+    console.log(`Cleaning dist folder...`)
+    fs.rmSync(dist, { recursive: true, force: true })
+  }
+}
+
+export function printDone() {
+  console.log('✅ Build done!')
+}
