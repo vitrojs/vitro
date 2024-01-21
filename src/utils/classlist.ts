4d68d2a0d6a753f97fb2be80401b45ecfae009bf
dong.huo <huodon@gmail.com>
2024-01-21 21:36:41 +0800

    apply lint rules

--- src/utils/classlist.ts
+++ src/utils/classlist.ts
@@ -7,49 +7,50 @@ import { isString } from './lang'
 // This function exists to optimize memory usage in some cases, where the classList API won't be touched without sacrificing performance
 
 const classesToggle = (
-  element: HTMLElement,
-  classes: string,
-  force: null | undefined | boolean,
+	element: HTMLElement,
+	classes: string,
+	force: null | undefined | boolean,
 ): void => {
-  const { className } = element
+	const { className } = element
 
-  /* OPTIMIZED PATH */
+	/* OPTIMIZED PATH */
 
-  if (isString(className)) {
-    if (!className) {
-      // Optimized addition/deletion
+	if (isString(className)) {
+		if (!className) {
+			// Optimized addition/deletion
 
-      if (force) {
-        // Optimized addition
+			if (force) {
+				// Optimized addition
 
-        element.className = classes
+				element.className = classes
 
-        return
-      } else {
-        // Optimized deletion, nothing to do really
+				return
+			}
+			// Optimized deletion, nothing to do really
 
-        return
-      }
-    } else if (!force && className === classes) {
-      // Optimized deletion
+			return
+		}
+		if (!force && className === classes) {
+			// Optimized deletion
 
-      element.className = ''
+			element.className = ''
 
-      return
-    }
-  }
+			return
+		}
+	}
 
-  /* REGULAR PATH */
+	/* REGULAR PATH */
 
-  if (classes.includes(' ')) {
-    classes.split(' ').forEach((cls) => {
-      if (!cls.length) return
+	if (classes.includes(' ')) {
+		// biome-ignore lint/complexity/noForEach: <explanation>
+		classes.split(' ').forEach((cls) => {
+			if (!cls.length) return
 
-      element.classList.toggle(cls, !!force)
-    })
-  } else {
-    element.classList.toggle(classes, !!force)
-  }
+			element.classList.toggle(cls, !!force)
+		})
+	} else {
+		element.classList.toggle(classes, !!force)
+	}
 }
 
 /* EXPORT */
